a75cb96c844d30518197031ab887438bcded4c98
solosky <solosky772@qq.com>
2023-11-25 13:41:12 +0800

    feed pwr mgmt when led on and off

--- firmware/application/src/rfid_port.h
+++ firmware/application/src/rfid_port.h
@@ -2,11 +2,12 @@
 #define RFID_PORT_H
 
 #include "bsp.h"
+#include "nrf_pwr_mgmt.h"
 
 #define ALIGN_U32  __attribute__((aligned(4)))
 
-#define TAG_FIELD_LED_OFF()  bsp_board_led_off(BSP_BOARD_LED_0);
-#define TAG_FIELD_LED_ON()  bsp_board_led_on(BSP_BOARD_LED_0);
+#define TAG_FIELD_LED_OFF()  bsp_board_led_off(BSP_BOARD_LED_0);nrf_pwr_mgmt_feed();
+#define TAG_FIELD_LED_ON()  bsp_board_led_on(BSP_BOARD_LED_0);nrf_pwr_mgmt_feed();
 
 #define set_slot_light_color(a)
 #define sleep_timer_start(a)
